6dad55b9ec7a6d5aadd8ea4436b81aad9910e4b8
cgonzale27 <cris27_1@hotmail.com>
2026-03-12 13:58:13 -0700

    Finalizar los dos loops
    Utilizamos comparaciones y variable para saber si se cumple o no un bucle y finalizamos escribiendo un resultado en el arreglo R, este. Bucle es infinito por el while(1) del inicio

--- DoubleLoop.c
+++ DoubleLoop.c
@@ -12,16 +12,35 @@ q: .word 0; #recorrido 60 a3
 .text
 
 lui t0, 10000
-
+  
   addi a0, zero, zero #r
+  addi a1, zero, zero #i
+  addi a2, zero, zero #j
   addi a3, zero, zero #q
-  addi t4, zero, zero #i
+  
+MAIN 
+
+  lw s1, 20(t0) #inicio arreglo P
+  lw t2, 0(t0) #inicio arreglo M
+  lw t4, 32(t0) #inicio arreglo R
+BUCLE1
+  slti t5, a2, 3 #j<3; t1=1
+  beq t5, x0, BUCLE2 #t1=0 va a bucle2 porque i>=3 y se rompe el bucle
+  lw t6, 0(s1) #cargar valor del arreglo en la memoria
+  add a3, a3, t6 #q=P[j]
+  addi a2, a2, 1 #j++
+  addi s1, s1, 4 #recorrer siguiente posicion de memoria del arreglo
+
+  BUCLE2
+  slti t1, a1, 5 #i<5; t1=1 sigue el bucle
+  beq t1, x0, FINAL #si t1=0 se rompio el for
+  lw t3, 0(t2) #t3=M[i]
+  addi a0, a3, t3 #r=q+M[i]
+  addi a1, a1, 1 #i++
+  addi t2, t2, 4 #Mover t2 al siguiente espacio de memoria de M[]
+  bne t1, x0, BUCLE2
 
   FINAL
-  lw t1, 0(t0)
-  add a0, a3, t1 #r=q+M[i]
-  addi t4, t4, 1 #i++
-  add t0, t0, 4 #recorrido para apunte a la siguiente memoria
-  blt t4, t5, FINAL
-
-  
+  sw a0, 0(t4) #R[j]=r
+  add a0, zero, zero #r=0
+  bne t5, x0 BUCLE1 #Si t5=1 i<3 sige bucle
